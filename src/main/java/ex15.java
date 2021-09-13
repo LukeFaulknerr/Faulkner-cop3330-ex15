27805cffd66e1bc1ff3b9f1e8fd3ec2b976f5465
LukeFaulknerr <lukefaulkner2002@knights.ucf.edu>
2021-09-12 22:42:14 -0400

    Initial commit

---
+++ src/main/java/ex15.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+/*
+    UCF COP3330 Fall 2021 Assignment 10 Solution
+    Copyright 2021 Luke Faulkner
+ */
+public class ex15 {
+    public static void main(String[] args) {
+
+        //Variables
+        String pw = "abc$123";
+        String input;
+        Scanner inputReader = new Scanner(System.in);
+
+        // Prompt
+        System.out.print("What is the password? ");
+        input = inputReader.nextLine();
+
+        // Result
+        if (pw.equals(input))
+            System.out.print("Welcome!");
+        else
+            System.out.print("I don't know you.");
+    }
+}
